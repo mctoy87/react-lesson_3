598f84c202e3abbd2ecd6c54cf6499b42e9eed2c
mctoy87 <mctoy87@mail.ru>
2023-12-21 14:59:59 +0200

    lifeCycle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import ComponentClass from './components/ClassComponent';
+// import ComponentClass from './components/ClassComponent';
 // import Button from './components/Button';
+import LifeCycle from './components/LifeCycle';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='app'>
-        <ComponentClass min={1} max={10}/>
+        {/* <ComponentClass min={1} max={10}/> */}
         {/* <Button isHide={true} text={'Сыграть еще?'}/> */}
+        <LifeCycle prop="METHED"/>
       </div>
     );
   }
